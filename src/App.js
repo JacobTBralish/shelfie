f4fd12af96073fc269bbe6138544bbc87a694020
Jacob Bralish <jacob@Inspectah-Ill.local>
2018-08-24 10:00:11 -0700

    Set up .env, created folders, installed necessary installations

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      
+    }
+  }
+
+
+
+
+
+
+  render() {
+
+
+
+
+
+
+    return (
+      <div className="App">
+ 
+      </div>
+    );
+  }
+}
+
+export default App;
